8c18c75c14b2c6d791d9629acec99f1d2c9425ec
bill <billionprince@gmail.com>
2013-10-16 14:24:57 +0800

    default website 2nd

---
+++ Control/DatabaseServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RGDZY.Control
+{
+    //connect database
+    public class DatabaseServer
+    {
+    }
+}
