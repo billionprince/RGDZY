0227791e2188d42c970738c4e69af14a2b44c7fe
titanxxh <xxhxxh@163.com>
2013-12-23 10:27:03 +0800

    add project_list feature

---
+++ data/project_list.ashx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using RGDZY.control;
+using System.Data.Linq;
+using System.Data.SqlClient;
+
+namespace RGDZY.data
+{
+    /// <summary>
+    /// Summary description for project
+    /// </summary>
+    public class project_list : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string command = context.Request["command"];
+            if (command != null)
+            {
+                System.Reflection.MethodInfo method = this.GetType().GetMethod(command);
+                if (method != null)
+                {
+                    method.Invoke(this, new object[] { context });
+                    return;
+                }
+            }
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Error");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void add_project_settings(HttpContext context)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            DataContext dc = DBConnectionSingleton.Instance.BorrowDBConnection();
+
+            try
+            {
+                var table = dc.GetTable<Project>();
+                Project myinfo = new Project();
+                myinfo.Id = 0;
+                myinfo.Name = context.Request["briefname"];
+                myinfo.FullName = context.Request["fullname"];
+                myinfo.Description = context.Request["description"];
+                myinfo.Link = context.Request["hyperlink"];
+                table.InsertOnSubmit(myinfo);
+                dc.SubmitChanges();
+
+                context.Response.ContentType = "json";
+                context.Response.Write(jss.Serialize(myinfo));
+            }
+            catch (System.Exception ex)
+            {
+                string msg = "Error occured while executing add_project_settings:";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            finally
+            {
+                DBConnectionSingleton.Instance.ReturnDBConnection(dc);
+            }
+        }
+
+        public void edit_project_settings(HttpContext context)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            DataContext dc = DBConnectionSingleton.Instance.BorrowDBConnection();
+
+            try
+            {
+                string idstr = context.Request["id"].ToString();
+                int id = int.Parse(idstr);
+                var query = from u in dc.GetTable<Project>()
+                            where u.Id == id
+                            select u;
+                var myinfo = query.First();
+                myinfo.Name = context.Request["briefname"];
+                myinfo.FullName = context.Request["fullname"];
+                myinfo.Description = context.Request["description"];
+                myinfo.Link = context.Request["hyperlink"];
+                dc.SubmitChanges();
+
+                context.Response.ContentType = "json";
+                context.Response.Write(jss.Serialize(myinfo));
+            }
+            catch (System.Exception ex)
+            {
+                string msg = "Error occured while executing edit_project_settings:";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            finally
+            {
+                DBConnectionSingleton.Instance.ReturnDBConnection(dc);
+            }
+        }
+
+        public void delete_project_settings(HttpContext context)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            DataContext dc = DBConnectionSingleton.Instance.BorrowDBConnection();
+
+            try
+            {
+                int id = int.Parse(context.Request["id"]);
+                var table = dc.GetTable<Project>();
+                var x = table.First(c => c.Id == id);
+                table.DeleteOnSubmit(x);
+                dc.SubmitChanges();
+
+                context.Response.ContentType = "json";
+                context.Response.Write(null);
+            }
+            catch (System.Exception ex)
+            {
+                string msg = "Error occured while executing edit_project_settings:";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            finally
+            {
+                DBConnectionSingleton.Instance.ReturnDBConnection(dc);
+            }
+        }
+
+        public void get_project_settings(HttpContext context)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            List<Dictionary<string, object>> rec = new List<Dictionary<string, object>>();
+            DataContext dc = DBConnectionSingleton.Instance.BorrowDBConnection();
+
+            try
+            {
+                var query = from u in dc.GetTable<Project>()
+                            select u;
+                foreach (var obj in query)
+                {
+                    Dictionary<string, object> evt = new Dictionary<string, object>();
+                    evt.Add("Id", obj.Id.ToString());
+                    evt.Add("BriefName", obj.Name);
+                    evt.Add("FullName", obj.FullName);
+                    evt.Add("Description", obj.Description);
+                    evt.Add("Hyperlink", obj.Link);
+                    rec.Add(evt);
+                }
+
+                context.Response.ContentType = "json";
+                context.Response.Write(jss.Serialize(rec));
+            }
+            catch (System.Exception ex)
+            {
+                string msg = "Error occured while executing get_project_settings:";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            finally
+            {
+                DBConnectionSingleton.Instance.ReturnDBConnection(dc);
+            }
+        }
+    }
+}
---
+++ media/js/project_list.js
@@ -0,0 +1,217 @@
+﻿var PROJECTS = function () {
+    return {
+        //main function to initiate the module
+        init: function () {
+            var oTable = $('#sample_editable_1').dataTable({
+                "aLengthMenu": [
+                    [5, 15, 20, -1],
+                    [5, 15, 20, "All"] // change per page values here
+                ],
+                // set the initial value
+                "iDisplayLength": 5,
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                "sPaginationType": "bootstrap",
+                "oLanguage": {
+                    "sLengthMenu": "_MENU_ records per page",
+                    "oPaginate": {
+                        "sPrevious": "Prev",
+                        "sNext": "Next"
+                    }
+                },
+                "aoColumnDefs": [{
+                    'bVisible': false,
+                    "bSearchable": false,
+                    'bSortable': false,
+                    'aTargets': [0]
+                }
+                ],
+            });
+
+            jQuery('#sample_editable_1_wrapper .dataTables_filter input').addClass("m-wrap medium"); // modify table search input
+            jQuery('#sample_editable_1_wrapper .dataTables_length select').addClass("m-wrap small"); // modify table per page dropdown
+            jQuery('#sample_editable_1_wrapper .dataTables_length select').select2({
+                showSearchInput: false //hide search box with special css class
+            }); // initialzie select2 dropdown
+
+            var nEditing = 0;
+
+            var isCreate = true;
+
+            function get_project_settings() {
+                $.ajax({
+                    url: "data/project_list.ashx",
+                    type: "POST",
+                    dataType: "json",
+                    data: {
+                        command: "get_project_settings"
+                    },
+                    success: function (rec) {
+                        for (var i = 0; i < rec.length; i++) {
+                            oTable.fnAddData([parseInt(rec[i].Id)
+                                , rec[i].BriefName
+                                , rec[i].FullName
+                                , rec[i].Description
+                                , rec[i].Hyperlink
+                                , '<a class="edit" href="#form_modal1" data-toggle="modal">Edit</a>'
+                                , '<a class="delete" data-mode="new" href = "javascript:">Delete</a>'
+                            ]);
+                        }
+                    },
+                    error: function () {
+                        alert("get_project_settings error!");
+                    }
+                });
+            }
+
+            get_project_settings();
+
+            //add Project
+            function addProject() {
+                $.ajax({
+                    type: "POST",
+                    url: "data/project_list.ashx",
+                    cache: false,
+                    dataType: 'json',
+                    data: {
+                        command: 'add_project_settings',
+                        briefname: $('#briefname').val(),
+                        fullname: $('#fullname').val(),
+                        description: $('#description').val(),
+                        hyperlink: $('#hyperlink').val()
+                    },
+                    success: function (rec) {
+                        oTable.fnAddData([parseInt(rec.Id)
+                            , rec.Name
+                            , rec.FullName
+                            , rec.Description
+                            , rec.Link
+                            , '<a class="edit" href="#form_modal1" data-toggle="modal">Edit</a>'
+                            , '<a class="delete" data-mode="new" href = "javascript:">Delete</a>'
+                        ]);
+                    },
+
+                    error: function (rec) {
+                        alert("add_project_settings error!");
+                    }
+                });
+            }
+
+            //edit Project
+            function editProject() {
+                $.ajax({
+                    type: "POST",
+                    url: "data/project_list.ashx",
+                    cache: false,
+                    dataType: 'json',
+                    data: {
+                        command: 'edit_project_settings',
+                        id: $('#projectid').val(),
+                        briefname: $('#briefname').val(),
+                        fullname: $('#fullname').val(),
+                        description: $('#description').val(),
+                        hyperlink: $('#hyperlink').val()
+                    },
+                    success: function (rec) {
+                        var aData = oTable.fnGetData(nEditing);
+                        //alert(aData[1]);
+                        aData[0] = rec.Id;
+                        aData[1] = rec.Name;
+                        aData[2] = rec.FullName;
+                        aData[3] = rec.Description;
+                        aData[4] = rec.Link;
+
+                        for (var i = 0, iLen = aData.length; i < iLen; i++) {
+                            oTable.fnUpdate(aData[i], nEditing, i, false);
+                        }
+                    },
+
+                    error: function (rec) {
+                        alert("edit_project_settings error!");
+                    }
+                });
+            }
+
+            //edit Project
+            function deleteProject(id) {
+                $.ajax({
+                    type: "POST",
+                    url: "data/project_list.ashx",
+                    cache: false,
+                    dataType: 'json',
+                    data: {
+                        command: 'delete_project_settings',
+                        id: parseInt(id)
+                    },
+                    success: function (rec) {
+                        //alert("delete_success");
+                    },
+
+                    error: function (rec) {
+                        alert("delete_project_settings error!");
+                    }
+                });
+            }
+
+            $('#save').click(function (e) {
+                if (isCreate) {
+                    addProject();
+                } else {
+                    editProject();
+                }
+            });
+
+            $('#sample_editable_1_new').click(function (e) {
+                e.preventDefault();
+                $("#project_form")[0].reset();
+                isCreate = true;
+            });
+
+            $('#sample_editable_1 a.delete').live('click', function (e) {
+                e.preventDefault();
+
+                if (confirm("Are you sure to delete this row ?") == false) {
+                    return;
+                }
+
+                var nRow = $(this).parents('tr')[0];
+                var aData = oTable.fnGetData(nRow);
+                deleteProject(aData[0]);
+                oTable.fnDeleteRow(nRow);
+            });
+
+            $('#sample_editable_1 a.cancel').live('click', function (e) {
+                e.preventDefault();
+                if ($(this).attr("data-mode") == "new") {
+                    var nRow = $(this).parents('tr')[0];
+                    oTable.fnDeleteRow(nRow);
+                } else {
+                    restoreRow(oTable, nEditing);
+                    nEditing = null;
+                }
+            });
+
+            $('#sample_editable_1 a.edit').live('click', function (e) {
+                e.preventDefault();
+
+                $("#project_form")[0].reset();
+                isCreate = false;
+
+                /* Get the row as a parent of the link that was clicked on */
+                var nRow = $(this).parents('tr')[0];
+                nEditing = nRow;
+
+                var aData = oTable.fnGetData(nRow);
+                var jqTds = $('>td', nRow);
+
+                $('#projectid').val(aData[0]);
+                $('#briefname').val(aData[1]);
+                $('#fullname').val(aData[2]);
+                $('#description').val(aData[3]);
+                $('#hyperlink').val(aData[4]);
+            });
+        }
+    };
+}();
+
+$(document).ready(function () {
+});
