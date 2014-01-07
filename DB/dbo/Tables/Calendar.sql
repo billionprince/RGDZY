f7af35d0ac6f5a0b65046f9403e32e48d4710fa7
titanxxh <xxhxxh@163.com>
2014-01-07 20:19:35 +0800

    Add project milestone in timeline page

--- DB/dbo/Tables/Calendar.sql
+++ DB/dbo/Tables/Calendar.sql
@@ -1,15 +1,17 @@
-﻿CREATE TABLE [dbo].[Calendar]
-(
-	[Id] UNIQUEIDENTIFIER NOT NULL , 
-    [type] INT NOT NULL, 
-    [owner] VARCHAR(50) NOT NULL, 
-    [title] VARCHAR(50) NOT NULL, 
-    [start_time] VARCHAR(50) NULL, 
-    [end_time] VARCHAR(50) NULL, 
-    [allday] INT NULL, 
-    [creator] VARCHAR(50) NULL, 
-    [url] VARCHAR(100) NULL, 
-    [detail] VARCHAR(100) NULL, 
-	[sendemail] varchar(50) NULL,
-    PRIMARY KEY ([owner], [Id], [type])
-)
+﻿CREATE TABLE [dbo].[Calendar] (
+    [Id]         UNIQUEIDENTIFIER NOT NULL,
+    [type]       INT              NOT NULL,
+    [owner]      VARCHAR (50)     NOT NULL,
+    [title]      VARCHAR (50)     NOT NULL,
+    [start_time] VARCHAR (50)     NULL,
+    [end_time]   VARCHAR (50)     NULL,
+    [allday]     INT              NULL,
+    [creator]    VARCHAR (50)     NULL,
+    [url]        VARCHAR (100)    NULL,
+    [detail]     VARCHAR (100)    NULL,
+    [sendemail]  VARCHAR (50)     NULL,
+    [group]      VARCHAR (50)     NULL,
+    CONSTRAINT [PK__Calendar__05E49D2446E78A0C] PRIMARY KEY CLUSTERED ([owner] ASC, [Id] ASC, [type] ASC)
+);
+
+
---
+++ DB/dbo/Tables/Milestone.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Milestone]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProjectId] INT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Time] DATETIME NULL, 
+    [ImagePath] NVARCHAR(MAX) NULL
+)
